8331b33a1f294e1030e07a55a2404d327c8becec
AmirArreaza <ajaarreazav@gmail.com>
2020-09-08 15:49:51 +0100

    Combining multiple reducers

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,8 +1,7 @@
-import * as actionTypes from './actions';
+import * as actionTypes from '../actions';
 
 const intialState = {
   counter: 0,
-  results: [],
 };
 const reducer = (state = intialState, action) => {
   switch (action.type) {
@@ -26,24 +25,7 @@ const reducer = (state = intialState, action) => {
         ...state,
         counter: state.counter - action.value,
       };
-    case actionTypes.STORE_RESULT:
-      return {
-        ...state,
-        //push will mutate the object not advisable to use
-        results: state.results.concat({ id: new Date(), value: state.counter }),
-      };
-    case actionTypes.DELETE_RESULT:
-      //const newArray =[...state.results];
-      //newArray.splice(id, 1);      
-
-      //Copy of the array
-      const updatedArray = state.results.filter(result => result.id !== action.resultId);
-      return{
-        ...state,
-        results: updatedArray
-      }
   }
-
   return state;
 };
 export default reducer;
---
+++ src/store/reducers/result.js
@@ -0,0 +1,28 @@
+import * as actionTypes from '../actions';
+
+const intialState = {
+  results: [],
+};
+const reducer = (state = intialState, action) => {
+  switch (action.type) {
+    case actionTypes.STORE_RESULT:
+      return {
+        ...state,
+        //push will mutate the object not advisable to use
+        results: state.results.concat({ id: new Date(), value: action.result }),
+      };
+    case actionTypes.DELETE_RESULT:
+      //const newArray =[...state.results];
+      //newArray.splice(id, 1);      
+
+      //Copy of the array
+      const updatedArray = state.results.filter(result => result.id !== action.resultId);
+      return{
+        ...state,
+        results: updatedArray
+      }
+  }
+
+  return state;
+};
+export default reducer;
